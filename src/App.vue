879f06a12e4f0efbe47e7dff28f5bff1c7c4de4c
evermake <vladeryabkin@gmail.com>
2024-10-29 17:28:11 +0500

    init repo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  Telegram.WebApp.MainButton.setParams({
+    color: '#000000',
+    text_color: '#ffffff',
+    has_shine_effect: true,
+    is_active: true,
+    is_visible: true,
+    position: 'bottom',
+    text: 'Close Me',
+  });
+  Telegram.WebApp.MainButton.onClick(() => {
+    Telegram.WebApp.close();
+  });
+});
+</script>
+
+<template>
+  <div>
+    <h1>Hi!!!</h1>
+  </div>
+</template>
